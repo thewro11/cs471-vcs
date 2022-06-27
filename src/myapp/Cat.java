d15a5cb209197f73e9b8f8675d44562b1171dc4c
Teen Thiti <thiti.thaw@ku.th>
2022-06-27 12:43:08 +0700

    bring back src/myapp/Cat.java from commit 0c1be62

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -3,6 +3,5 @@ package myapp;
 public class Cat {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("This is a Cat class.");
     }
 }
