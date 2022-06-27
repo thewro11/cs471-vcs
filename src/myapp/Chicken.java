265ea73f1ca4f7f57bb394432ff81c2c4b238b9d
Teen Thiti <thiti.thaw@ku.th>
2022-06-27 12:56:20 +0700

    Add Dog

---
+++ src/myapp/Chicken.java
@@ -0,0 +1,8 @@
+package myapp;
+
+public class Chicken {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is a Chicken class.");
+    }
+}
