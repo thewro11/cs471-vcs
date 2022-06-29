a2b7dc60422d21f613954a0e00bcd9a924e9b6fc
Théo Teenster <thewro11@gmail.com>
2022-06-29 13:54:23 +0700

    update z

--- src/myapp/Z.java
+++ src/myapp/Z.java
@@ -2,6 +2,6 @@ package myapp;
 
 public class Z {
     public static void main(String[] args) {
-        System.out.println("This is a Y class.");
+        System.out.println("This is a Z class.");
     }
 }
